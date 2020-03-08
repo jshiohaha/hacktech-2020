bcd683f0fd247a9a29f6416c24c5261dd9590d0c
Jacob Shiohira <jlshiohira23@gmail.com>
2020-03-08 08:26:37 -0500

    Update app.tsx with firebase functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,159 @@
 import React from "react";
 import "./App.css";
 
+import firebase from "firebase/app";
+import "firebase/firestore";
+
 import Button from "react-bootstrap/Button";
 import CardColumns from "react-bootstrap/CardColumns";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
 
-import LoadingButton from "./components/LoadingButton/LoadingButton";
+import { toast } from "react-toastify";
+
+import SearchBar from "./components/SearchBar/SearchBar";
 import QuestionModal from "./components/QuestionModal/QuestionModal";
 
 import { Answer } from "./components/Answer/Answer.types";
-import QueryComponent from "./components/Query/QueryComponent";
-import { Query } from "./components/Query/Query.types";
+import QuestionComponent from "./components/QuestionComponent/QuestionComponent";
+import { Question } from "./components/Question/Question.types";
+import { QUESTIONS_DATABASE } from "./config/dbName";
 
-function App() {
-    const [questionModalShow, setQuestionModalShow] = React.useState(false);
+// TODO: pass in or load answers
+interface IAppProps {}
 
-    const data = {
-      "question": "Is this a question?",
-      "metaText": "This is question meta-text",
-      "acceptedAnswer": "",
-      "answers": [] as Answer[]
-    };
-
-    const submitQuestion = (questionText: string) => {
-      console.log(questionText);
-      return;
-    };
-
-    const submitAnswer = (answer: string, source: string) => {
-      console.log("answer: ", answer);
-      console.log("source: ", source);
-
-      return;
-    };
-
-
-    /** TODOs
-     * 
-     * - Add last updated time
-     * 
-     * - Change the answer data passed into the modal! 
-     */
-    return (
-        <>
-            <div className="App">
-                <Button
-                    variant="primary"
-                    style={{ float: "right", position: "relative" }}
-                    onClick={() => setQuestionModalShow(true)}
-                >
-                    Ask a Question
-                </Button>
-                <div className="search-bar-container">
-                    <InputGroup className="mb-3">
-                        <FormControl
-                            placeholder="🔍 Search All Questions"
-                            aria-label="Search Text Input"
-                            aria-describedby="basic-addon2"
-                        />
-                        <InputGroup.Append>
-                            <LoadingButton />
-                        </InputGroup.Append>
-                    </InputGroup>
-                </div>
-
-                <CardColumns>
-                  {
-                    [""].map(x => {
-                      return <QueryComponent
-                        queryEntity={new Query(data)}
-                        submitQueryAnswer={submitAnswer}
-                      />
-                    })
-                  }
-                </CardColumns>
-            </div>
-
-            <QuestionModal
-                show={questionModalShow}
-                onHide={() => setQuestionModalShow(false)}
-                submitQuestion={submitQuestion}
-            />  
-        </>
-    );
+interface IAppState {
+    questionEntities: Question[];
+    questionModalShow: boolean;
 }
 
-export default App;
+export default class App extends React.Component<IAppProps, IAppState> {
+    // instantiate firebase handlers
+    constructor(props: IAppProps) {
+        super(props);
+        this.state = {
+            questionEntities: [] as Question[],
+            questionModalShow: false
+        };
+    }
+
+    loadQuestions = () => {
+        firebase
+            .firestore()
+            .collection(QUESTIONS_DATABASE)
+            .onSnapshot(querySnapshot => {
+                this.setState({
+                    questionEntities: [] as Question[]
+                });
+                querySnapshot.forEach(doc => {
+                    let data = doc.data();
+                    data["id"] = doc.id;
+                    const question: Question = new Question(data);
+                    this.setState({
+                        questionEntities: [
+                            ...this.state.questionEntities,
+                            question
+                        ]
+                    });
+                });
+            });
+    };
+
+    submitQuestion = (question: string, desc: string) => {
+        // Add a new document with a generated id.
+        firebase
+            .firestore()
+            .collection(QUESTIONS_DATABASE)
+            .add({
+                keywords: question.split(" "),
+                question: question,
+                description: desc,
+                answers: [] as Answer[]
+            })
+            .then(() => {
+                toast.success(
+                    'Successfully added question: "' + question + '"',
+                    {
+                        position: toast.POSITION.TOP_CENTER
+                    }
+                );
+            })
+            .then(() => {
+                this.loadQuestions();
+            })
+            .catch(function(error) {
+                toast.error('Error adding question: "' + question + '"', {
+                    position: toast.POSITION.TOP_CENTER
+                });
+            });
+
+        return;
+    };
+
+    setQuestionModalShow = (targetState: boolean) => {
+        this.setState({
+            questionModalShow: targetState
+        });
+    };
+
+    componentDidMount() {
+        this.loadQuestions();
+    }
+
+    clearSearchResults = () => {
+        this.loadQuestions();
+    }
+
+    searchFunction = async (query: string) => {
+        const snapshot = await firebase.firestore().collection(QUESTIONS_DATABASE)
+            .where('keywords', 'array-contains', query.toLowerCase())
+            .get();
+        console.log(snapshot);
+        const results: Question[] = snapshot.docs.map(doc => {
+            return new Question(doc.data());
+        });
+
+        this.setState({
+            questionEntities: results
+        });
+    };
+
+    /** TODOs
+     *
+     * - Add last updated time
+     *
+     * - Change the answer data passed into the modal!
+     */
+    render() {
+        return (
+            <>
+                <div className="App">
+                    <Button
+                        variant="primary"
+                        style={{ float: "right", position: "relative" }}
+                        onClick={() => this.setQuestionModalShow(true)}
+                    >
+                        Ask a Question
+                    </Button>
+
+                    <SearchBar searchFunction={this.searchFunction} clearSearchResults={this.clearSearchResults} />
+
+                    <CardColumns>
+                        {/* TODO: add a default state when there are no questions? */}
+                        {this.state.questionEntities.map(question => {
+                            return (
+                                <QuestionComponent
+                                    questionEntity={question}
+                                />
+                            );
+                        })}
+                    </CardColumns>
+                </div>
+
+                <QuestionModal
+                    show={this.state.questionModalShow}
+                    onHide={() => this.setQuestionModalShow(false)}
+                    submitQuestion={this.submitQuestion}
+                />
+            </>
+        );
+    }
+}
