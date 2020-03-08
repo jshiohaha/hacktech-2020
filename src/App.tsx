6b09cb7e006be2862acbc8b3abd93a504eb06bc8
Jacob Shiohira <jlshiohira23@gmail.com>
2020-03-08 01:08:18 -0600

    Add toast messages and update global stylings

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,88 @@
 import React from "react";
-// import logo from "./logo.svg";
 import "./App.css";
 
-import Button from 'react-bootstrap/Button'
-import Card  from "react-bootstrap/Card";
-import CardColumns from 'react-bootstrap/CardColumns';
+import Button from "react-bootstrap/Button";
+import CardColumns from "react-bootstrap/CardColumns";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+
+import LoadingButton from "./components/LoadingButton/LoadingButton";
+import QuestionModal from "./components/QuestionModal/QuestionModal";
+
+import { Answer } from "./components/Answer/Answer.types";
+import QueryComponent from "./components/Query/QueryComponent";
+import { Query } from "./components/Query/Query.types";
 
 function App() {
+    const [questionModalShow, setQuestionModalShow] = React.useState(false);
+
+    const data = {
+      "question": "Is this a question?",
+      "metaText": "This is question meta-text",
+      "acceptedAnswer": "",
+      "answers": [] as Answer[]
+    };
+
+    const submitQuestion = (questionText: string) => {
+      console.log(questionText);
+      return;
+    };
+
+    const submitAnswer = (answer: string, source: string) => {
+      console.log("answer: ", answer);
+      console.log("source: ", source);
+
+      return;
+    };
+
+
+    /** TODOs
+     * 
+     * - Add last updated time
+     * 
+     * - Change the answer data passed into the modal! 
+     */
     return (
-        <div className="App">
-            <CardColumns>
-                <Card border="success">
-                    <Card.Body>
-                        <Card.Title>
-                            Card title that wraps to a new line
-                        </Card.Title>
-                        <Card.Text>
-                            This is a longer card with supporting text below as
-                            a natural lead-in to additional content. This
-                            content is a little bit longer.
-                        </Card.Text>
+        <>
+            <div className="App">
+                <Button
+                    variant="primary"
+                    style={{ float: "right", position: "relative" }}
+                    onClick={() => setQuestionModalShow(true)}
+                >
+                    Ask a Question
+                </Button>
+                <div className="search-bar-container">
+                    <InputGroup className="mb-3">
+                        <FormControl
+                            placeholder="🔍 Search All Questions"
+                            aria-label="Search Text Input"
+                            aria-describedby="basic-addon2"
+                        />
+                        <InputGroup.Append>
+                            <LoadingButton />
+                        </InputGroup.Append>
+                    </InputGroup>
+                </div>
 
-                        <Card.Text>
-                            <small className="text-muted">
-                                Last updated 3 mins ago
-                            </small>
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                      <Button variant="success" style={{ width: "100px", marginRight: "15px" }}>Agree</Button>
-                      <Button variant="danger" style={{ width: "100px" }}>Disagree</Button>
-                    </Card.Footer>
-                </Card>
+                <CardColumns>
+                  {
+                    [""].map(x => {
+                      return <QueryComponent
+                        queryEntity={new Query(data)}
+                        submitQueryAnswer={submitAnswer}
+                      />
+                    })
+                  }
+                </CardColumns>
+            </div>
 
-            </CardColumns>
-        </div>
+            <QuestionModal
+                show={questionModalShow}
+                onHide={() => setQuestionModalShow(false)}
+                submitQuestion={submitQuestion}
+            />  
+        </>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+// Call it once in your app. At the root of your app is the best place
+toast.configure({
+    autoClose: 5000,
+    draggable: false,
+    //etc you get the idea
+  });
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
